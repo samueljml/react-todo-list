8e0a61842581112bb56f96ae28f610e424cd64a6
Samuel Andrade <devsamuelandrade@gmail.com>
2021-10-24 16:27:19 -0300

    Add TodoList page structure

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,3 +1,21 @@
+import { FormEvent, useState } from "react";
+import { Input } from "../components/Input";
+import wording from "./TodolistWording.json";
+
 export const TodoList = () => {
-	return <div className="todo-list"></div>;
+	const [input, setInput] = useState("");
+
+	return (
+		<form className="todo-list container align-middle">
+			<h1 className="fs-3 mb-4">{wording.h1}</h1>
+			<div className="d-flex">
+				<Input
+					value={input}
+					setValue={setInput}
+					placeHolder={wording.input}
+				/>
+				<button className="btn">{wording.button}</button>
+			</div>
+		</form>
+	);
 };
